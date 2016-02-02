ca1e828d98a18bf7a7d8fbcf0059efebcc7d39cd
Matt MacManes <matthew.macmanes@unh.edu>
2016-02-02 13:33:51 -0500

    Update autoPAML.py

--- autoPAML.py
+++ autoPAML.py
@@ -3,11 +3,11 @@
 #usage  python autoPAML.py alignment.fasta tree.nexus paml.output
 
 from __future__ import division
-from Bio.Phylo.PAML import codeml ##Biopython PAML
+from Bio.Phylo.PAML import codeml
 import sys
 
 if len(sys.argv) != 4:
-        print "Error.  There should be three inputs. An alignment in fasta format, a tree file, and the name for the output file"
+        print "Error.  our missing an input file, see the usage"
         quit()
 
 cml = codeml.Codeml()
@@ -15,8 +15,4 @@ cml.read_ctl_file("codeml.ctl")
 cml.alignment = sys.argv[1]
 cml.tree = sys.argv[2]
 cml.out_file = sys.argv[3]
-
-name=sys.argv[1]
-print name
-
 cml.run()
