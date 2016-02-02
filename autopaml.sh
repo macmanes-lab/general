82674f27e6f5972fc17f11a6e9a39f5144dbe930
Matt MacManes <matthew.macmanes@unh.edu>
2016-02-02 11:47:50 -0500

    Update and rename align.sh to autopaml.sh

--- autopaml.sh
+++ autopaml.sh
@@ -4,7 +4,7 @@ usage=$(cat << EOF
    # This script runs a pipeline that takes a fasta file and BAMfiles and tests for selection:
    #
 
-   align.sh [options]
+   autopaml.sh [options]
    Options:
       -t <v> : *required* Numberof threads to use.
 EOF
@@ -31,9 +31,9 @@ for inputaln in $(ls *fasta); do
     if [ $(ps -all | grep 'java\|codeml\|raxmlHPC-PTHREADS' | wc -l | awk '{print $1}') -lt $TC ] ;
     then
         echo 'I have a core to use'
-        java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq $inputaln -out_NT $F.aln &
-        raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.aln -p 69283650 &
-        python codemlScript.py $F.aln RAxML_bestTree.$F.tree $F.out &
+        java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq $inputaln -out_NT $F.aln &&
+        raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.aln -p 69283650 &&
+        python autoPAML.py $F.aln RAxML_bestTree.$F.tree $F.out &
     else
         echo 'Dont wake me up until there is something else to do'
         sleep 25s
