a1e92ae682da4d7b880284d4c25f87e9ac29533e
Matt MacManes <matthew.macmanes@unh.edu>
2016-02-06 19:11:56 -0500

    add pal2nal

--- autoPAML.sh
+++ autoPAML.sh
@@ -32,8 +32,9 @@ for inputaln in $(ls *fasta); do
     then
         echo 'I have a core to use'
         java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq $inputaln -out_NT $F.aln &&
-        raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.aln -p 69283650 &&
-        python autoPAML.py $F.aln RAxML_bestTree.$F.tree $F.out &
+        perl /share/pal2nal.v14/pal2nal.pl $F_macse_AA.fasta $F.aln -output fasta -nogap -nomismatch > $F.clean &&
+        raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.clean -p 69283650 &&
+        python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &
     else
         echo 'Dont wake me up until there is something else to do'
         sleep 25s
