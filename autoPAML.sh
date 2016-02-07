bb7a7bc3e7ba70e2ca7192be3fe5507766c93705
Matt MacManes <matthew.macmanes@unh.edu>
2016-02-07 10:49:47 -0500

    Update autoPAML.sh

--- autoPAML.sh
+++ autoPAML.sh
@@ -32,7 +32,7 @@ for inputaln in $(ls *fasta); do
     then
         echo 'I have a core to use'
         java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq $inputaln -out_NT $F.aln &&
-        perl /share/pal2nal.v14/pal2nal.pl $F_macse_AA.fasta $F.aln -output fasta -nogap -nomismatch > $F.clean &&
+        perl /share/pal2nal.v14/pal2nal.pl $F'_macse_AA.fasta' $F.aln -output fasta -nogap -nomismatch > $F.clean &&
         raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.clean -p 69283650 &&
         python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &
     else
