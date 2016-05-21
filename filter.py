3f7e5b864deb2605ebc943d4cd10de8d584a79b6
Matt MacManes <matthew.macmanes@unh.edu>
2016-05-21 07:38:58 -0400

    remove not

--- filter.py
+++ filter.py
@@ -6,4 +6,4 @@ import sys
                                                                                     
 wanted = [line.strip() for line in open(sys.argv[2])]                               
 seqiter = SeqIO.parse(open(sys.argv[1]), 'fasta')                                    
-SeqIO.write((seq for seq in seqiter if seq.id not in wanted), sys.stdout, "fasta")
+SeqIO.write((seq for seq in seqiter if seq.id in wanted), sys.stdout, "fasta")
