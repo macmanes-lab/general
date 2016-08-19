ea290ede61a51b79fc59c44b3b5cfc2ece91b727
Matt MacManes <matthew.macmanes@unh.edu>
2016-08-19 13:39:15 -0400

    add usage and take input from sys_argv

--- length.py
+++ length.py
@@ -1,20 +1,25 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
-import sys 
+#USAGE: python merge_counts.py dry/ dry.counts
 
-file_open = open(sys.argv[1],'r').readlines() 
-file_out = open(sys.argv[2],'w') 
+import os
+import sys
+from os import path
+import pandas as pd
 
-switch = 0 
-for lines in file_open:
-    if '>' in lines:
-        if switch == 0:
-            file_out.write(lines.strip() + "\t")
-            seq_length = 0
-            switch = 1
-        elif switch == 1:
-            file_out.write(str(seq_length) + "\n" + lines.strip() + "\t")
-            seq_length = 0
-    else:
-        seq_length+=len(lines.strip())
-file_out.write(str(seq_length))
+
+# get txp names as index
+listofsamples = os.listdir(sys.argv[1])
+quants = {}
+data = None
+for sample in listofsamples:
+    if os.path.isdir(sys.argv[1]+sample):
+        if os.path.isfile(sys.argv[1]+sample+"/quant.sf"):
+            quant_file = sys.argv[1]+sample+"/quant.sf"
+            data=pd.DataFrame.from_csv(quant_file,sep='\t')
+            numreads = data['NumReads']
+            quants[sample] = numreads
+
+counts = pd.DataFrame.from_dict(quants)
+counts.set_index(data.index,inplace=True)
+counts.to_csv(sys.argv[2])
