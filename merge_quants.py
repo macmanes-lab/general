d9699958c8ab35bc36b601d2215dd7d751de4f3d
Matt MacManes <matthew.macmanes@unh.edu>
2016-08-23 08:30:08 -0400

    Update merge_quants.py

--- merge_quants.py
+++ merge_quants.py
@@ -1,24 +1,25 @@
-#this is originally from Lisa Cohen as part of ANGUS 2016
+#!/usr/bin/env python
 
-#USAGE: python merge_counts.py
+#USAGE: python merge_counts.py dry/ dry.counts
 
 import os
+import sys
 from os import path
 import pandas as pd
 
 
 # get txp names as index
-listofsamples = os.listdir("dry/")
+listofsamples = os.listdir(sys.argv[1])
 quants = {}
 data = None
 for sample in listofsamples:
-    if os.path.isdir("dry/"+sample):
-        if os.path.isfile("dry/"+sample+"/quant.sf"):
-            quant_file = "dry/"+sample+"/quant.sf"
+    if os.path.isdir(sys.argv[1]+sample):
+        if os.path.isfile(sys.argv[1]+sample+"/quant.sf"):
+            quant_file = sys.argv[1]+sample+"/quant.sf"
             data=pd.DataFrame.from_csv(quant_file,sep='\t')
             numreads = data['NumReads']
             quants[sample] = numreads
 
 counts = pd.DataFrame.from_dict(quants)
 counts.set_index(data.index,inplace=True)
-counts.to_csv("counts.csv")
+counts.to_csv(sys.argv[2])
